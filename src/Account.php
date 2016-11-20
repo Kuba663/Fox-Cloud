e80d212e3326cb29f77437e4d047f4e5324a4a99
Kuba663 <kuba63b@gmail.com>
2016-11-20 18:56:45 +0100

    Added Account Page

---
+++ src/Account.php
@@ -0,0 +1,31 @@
+<?php
+
+	session_start();
+	
+	if (!isset($_SESSION['zalogowany']))
+	{
+		header('Location: index.php');
+		exit();
+	}
+	
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Osadnicy - gra przeglądarkowa</title>
+</head>
+
+<body>
+	
+<?php
+
+	echo "<p>Witaj ".$_SESSION['user'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>'
+	echo " | <b>Liczba Instancji</b>: ".$_SESSION['Instancje']."</p>";
+	
+	echo "<p><b>E-mail</b>: ".$_SESSION['email'];
+?>
+
+</body>
+</html>
