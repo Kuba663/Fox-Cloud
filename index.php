b141a894828cd9f9fd4fc1602f3270a91c9f3173
Kuba663 <kuba63b@gmail.com>
2016-12-09 11:16:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     echo "<h1>Witamy na Fox Cloud</h1>"
     echo "Fox Cloud to narzędzie deweloperskie umożliwiaujące łatwe tworzenie chmur.\n Jednak postawienie serwera Hostingowego drogo kosztuje."
     echo "Możesz zacząć już teraz."
+    echo "<input type="button" value="zaloguj sie" action="goto_login.php"/>
   }
 ?>
 </body>
