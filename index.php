6866a31d2a9abfc674e5a5af2bd41b6535fd042c
Kuba663 <kuba63b@gmail.com>
2016-12-09 11:04:06 +0100

    Main Page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  setcookie("languange","pl-PL",time()+3600);
+?>
+<html>
+<head>
+<meta charset="UTF-8"></meta>
+</head>
+<body>
+<?php
+  if $_ COOKIE['languange'] = "pl-PL" 
+  {
+    echo "<h1>Witamy na Fox Cloud</h1>"
+  }
+?>
+</body>
